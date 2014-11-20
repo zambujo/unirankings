0b91fcdeedc4e45dbcce7e41edc742b300a1695f
zambujo <joao.martins@gmx.net>
2014-11-20 23:22:41 +0100

    added cwur.csv

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Studying the distribution of university rankings in the world.
 
 Files:
   - *qsrankings14.csv* [QS World University Rankings  2014/15](http://www.topuniversities.com/university-rankings/world-university-rankings/2014)
-  - *webometrics.csv* [ranking web of universities](http://webometrics.info/en/node/54)
+  - *cwur.csv* [Center for World University Rankings](http://cwur.org/2014/)
--- uniranks.R
+++ uniranks.R
@@ -4,34 +4,37 @@ dat=head (web, 7)
 countries=as.character(dat[, 1])
 totals=dat[, 8]
 dat=dat[,-c (1, 8)]
-
-perc=100*dat/totals
-rownames (perc)=countries
-head (countries, 7)
-matplot (t (perc), type = "b", lty=1, lwd=2)
 ## CH doesn't have 103 universities!
-## data not reliable
+## data is not reliable
 
+# perc=100*dat/totals
+# rownames (perc)=countries
+# head (countries, 7)
+# matplot (t (perc), type = "b", lty=1, lwd=2)
 
 dat=read.csv("qsrankings14.csv")
+
+## dirty extrapolation:
+## assume exponential decay of rankings
 x=1:sum (!is.na(dat[, 1]))
 y=dat[x, 1]
-fit1=lm(log (y)~x)
+fit=lm (log (y)~x, weights=x^10)
 
+## x-tended x
 xx=1:nrow (dat)
 
-plot (xx, dat[, 1], ylim=c (0, 100), type="l", lwd=5)
-preds <- exp(predict(fit1,data.frame(x=xx)))
-lines (xx[-x], (preds+2)[-x], lwd=5, col="red")
+preds <- exp (predict (fit, data.frame(x=xx[-x])))
+dat[xx[-x],1]=preds
 
-## dirty extrapolation
-dat[xx[-x],1]=(preds+2)[-x]
+plot (dat[, 1], ylim=c (0, 100), type="l", lwd=5)
+abline (v=length (x)+.5, lty=1, col='red')
 
 ## countries with most universities in the ranking
-countries=rev (names (tail (sort (table (dat[, 3])))))
+countries=names (tail (sort (table (dat[, 3]))))
+countries=c (countries, "Switzerland") # add CH
 
 library (dplyr)
-dat %>% filter(Country==countries[1]) %>% select (Score)
+dat %>% filter(Country%in%countries %>% select (Score)
 
 ## use wikipedia to check how many universities countries have
 ## http://en.wikipedia.org/wiki/Lists_of_universities_and_colleges_by_country
